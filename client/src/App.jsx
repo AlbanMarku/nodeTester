78487430c1d0181628a39386d924498e8e692025
AlbanMarku <albanmarku@outlook.com>
2022-11-17 21:11:46 +0000

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+import { useState } from 'react'
+
+function App() {
+  const [data, setData] = useState();
+
+  useEffect(()=> {
+    fetch("/api").then((res) => res.json()).then((data) => setData(data.message));
+    console.log("done fetching")
+  },[])
+
+  return (
+    <div className="App">
+      <p>hello world!</p>
+      <p>{data}</p>
+    </div>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express'),
+    path    = require('path'),
+    route   = require('./route.js'),
+    app     = express(),
+    port    = process.env.PORT || 8080;
+    
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
+
+route(app);
+
+app.listen(port);
+
+console.log(`API server is listening on port:${port}`);
